5e5a95bc86890078f9da07d6c037d2f5449a2a82
Heming <im.hemingzhang@gmail.com>
2019-12-17 17:44:49 -0600

    data lab init

--- data-lab/bits.c
+++ data-lab/bits.c
@@ -1,7 +1,7 @@
 /* 
  * CS:APP Data Lab 
  * 
- * <Heming Zhang Userid: 475982 >
+ * <Heming Zhang Userid: 475982>
  * 
  * bits.c - Source file with your solutions to the Lab.
  *          This is the file you will hand in to your instructor.
